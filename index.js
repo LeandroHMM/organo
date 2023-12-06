4b1687a848940bf89256cdd39b856b9f260ffd03
LeandroHMM <53102337+LeandroHMM@users.noreply.github.com>
2023-12-05 23:13:24 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+//import Colaborador from '../Colaborador';
+import Colaborador from "../Colaborador";
+import "./Time.css";
 
+const Time = (props) => {
+  return (
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    props.colaboradores.length > 0 ? <section className="time" style={{ backgroundColor: props.corDaFrente }}>
+      <h3 style={{ borderColor: props.corDoFundo }}>{props.nome}</h3>
 
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => 
+          <Colaborador
+            corDeFundo={props.corDoFundo}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        )}
+      </div>
+    </section>
+    : ""
+  );
+};
+
+export default Time;
